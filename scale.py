9162512f8cea75cccb054c623c447a0f3f77240f
H3R11 <el7dedurango@gmail.com>
2026-04-30 21:51:44 -0600

    Update scale.py

--- scale.py
+++ scale.py
@@ -126,7 +126,7 @@ def main():
         client = Client(token=access_token)
         for device in client.devices_list():
             if device.type == "WyzeScale" or device.product_model == "WL_SCU":
-:
+
                 scale = client.scales.info(device_mac=device.mac)
                 print(f"Scale found with MAC {device.mac}. Latest record is:")
                 print(scale.latest_records)
